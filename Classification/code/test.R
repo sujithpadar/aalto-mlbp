fe8d0f2a5b55a775b4fd32d9d47af0060bafb88c
hegdepashupati <pashupati.hegde@gmail.com>
2016-10-31 17:21:54 +0200

    testing

--- Classification/code/test.R
+++
@@ -1,9 +0,0 @@
-basedir <- "/media/Documents/01 Aalto/03 Study/Semester 01/02 Machine Learning Basic Principles - Alex Jung/Term Project/Classification/"
-setwd(basedir)
-
-data <- read.csv("data/classification_dataset_training.csv")
-formula <- as.formula(paste("rating~",paste(setdiff(colnames(data),c("ID","rating")),collapse = "+"),sep=""))
-
-mod <- glm(data=data,formula=formula,family = binomial("logit"))
-
-predict <- predict(mod,type="response")
